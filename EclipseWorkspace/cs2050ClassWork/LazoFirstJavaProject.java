7bd7ee644c3992fd2ec27490491370b74b65e8b3
jlazoort <jlazoortiz7@gmail.com>
2025-02-05 15:46:24 -0700

    Fixed IDE
    Fixed idle by adding java path and started program

---
+++ EclipseWorkspace/cs2050ClassWork/LazoFirstJavaProject.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class LazoFirstJavaProject {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			System.out.print("Hello");
+
+		}
+
+}
